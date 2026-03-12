98e280cedab0359a32a3ff62ad2b48bb6b325072
dangherve <dangherve@gmail.com>
2026-03-12 13:44:26 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/thrau/vnstat2-php-frontend.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/vnstat2-php-frontend.git
+git push --force --set-upstream origin master
